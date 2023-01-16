a027575229f77652bf126f2e925de35ac7b892c6
jack <jaksonmillka@gmail.com>
2023-01-16 16:36:38 +0100

    Add 8 variables in GameSystem class

--- src/main/java/GameSystem.java
+++ src/main/java/GameSystem.java
@@ -3,4 +3,15 @@
  * @link https://github.com/dublXq
  */
 public class GameSystem {
+
+    protected static final int Cube_ONE = 1;
+    protected static final int Cube_TWO = 2;
+    protected static final int Cube_THREE = 3;
+    protected static final int Cube_FOUR = 4;
+    protected static final int Cube_FIVE = 5;
+
+    int SummaOfAllNumbersPerson; // Общая сумма очков игрока
+    int SummaOfAllNumbersBot; // Общая сумма очков бота
+    int ViewSummaForAllPlayers; // Переменная отвечающая за временный вывод
+
 }
