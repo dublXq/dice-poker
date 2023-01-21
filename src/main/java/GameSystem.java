54a690e94afe1e7de7965d703fb358842551f20e
jack <jaksonmillka@gmail.com>
2023-01-21 22:45:09 +0100

    Optimisation all classes, and added playersBotScore() method in class GameSystem

--- src/main/java/GameSystem.java
+++ src/main/java/GameSystem.java
@@ -2,140 +2,160 @@ import java.util.Random;
 
 /**
  * @author Evgeniy_Tyslevich
- * @link https://github.com/dublXq
+ * @link <a href="https://github.com/dublXq">...</a>
  */
 public class GameSystem {
 
-    protected static final int Cube_ONE = 1;
-    protected static final int Cube_TWO = 2;
-    protected static final int Cube_THREE = 3;
-    protected static final int Cube_FOUR = 4;
-    protected static final int Cube_FIVE = 5;
-    protected static final int Cube_SIX = 6;
-
-    public int SummaOfAllNumbersPerson; // Общая сумма очков игрока
-    public int SummaOfAllNumbersBot; // Общая сумма очков бота
+    public final int CUBE_ONE = 1;
+    public final int CUBE_TWO = 2;
+    public final int CUBE_THREE = 3;
+    public final int CUBE_FOUR = 4;
+    public final int CUBE_FIVE = 5;
+    public final int CUBE_SIX = 6;
+    public static String area;
+    public static int summaOfAllNumbersPerson; // Общая сумма очков игрока
+    public static int summaOfAllNumbersBot; // Общая сумма очков бота
     int ViewSummaForAllPlayers; // Переменная отвечающая за временный вывод
-
     String randomCube;
-    String one =
-            "-----\n" +
-                    "|   |\n" +
-                    "| o |\n" +
-                    "|   |\n" +
-                    "-----";
-    String two =
-            "-----\n" +
-                    "|o  |\n" +
-                    "|   |\n" +
-                    "|  o|\n" +
-                    "-----";
-    String three =
-            "-----\n" +
-                    "|o  |\n" +
-                    "| o |\n" +
-                    "|  o|\n" +
-                    "-----";
-    String four =
-            "-----\n" +
-                    "|o o|\n" +
-                    "|   |\n" +
-                    "|o o|\n" +
-                    "-----";
-    String five =
-            "-----\n" +
-                    "|o o|\n" +
-                    "| o |\n" +
-                    "|o o|\n" +
-                    "-----";
 
-    String six =
-            "-----\n" +
-                    "|o o|\n" +
-                    "|o o|\n" +
-                    "|o o|\n" +
-                    "-----";
 
-    String area = "------------------------------------\n" +
-            "|\t\t\t\t      | Игрок | Бот |\n" +
-            "|_____________________|_______|_____|_____     \n" +
-            "|\t\t\t\t\t  |       |           |\n" +
-            "|      Единицы        |              <---  \n" +
-            "|_____________________|_______|___________|\n" +
-            "|\t\t\t\t\t  |       |           |\n" +
-            "|       Двойки        |              <---\n" +
-            "|_____________________|_______|___________|\n" +
-            "|\t\t\t\t\t  |       |           |\n" +
-            "|       Тройки        |              <--- \n" +
-            "|_____________________|_______|___________|\n" +
-            "|\t\t\t\t\t  |       |           |\n" +
-            "|      Четверки       |              <---  \n" +
-            "|_____________________|_______|___________|\n" +
-            "|\t\t\t\t\t  |       |           |\n" +
-            "|      Пятерки        |              <---  \n" +
-            "|_____________________|_______|___________|\n" +
-            "|\t\t\t\t\t  |       |           |\n" +
-            "|      Шестерки       |              <---  \n" +
-            "|_____________________|_______|___________|\n" +
-            "|\t\t\t\t\t  |       |           |\n" +
-            "|        Сумма        |              <---  \n" +
-            "|_____________________|_______|___________|\n" +
-            "|\t\t\t\t\t  |       |           |\n" +
-            "|        Бонус        |              <---  \n" +
-            "|_____________________|_______|___________|\n" +
-            "|\t\t\t\t\t  |       |           |\n" +
-            "|   Три одинаковых    |              <---  \n" +
-            "|_____________________|_______|___________|\n" +
-            "|\t\t\t\t\t  |       |           |\n" +
-            "|  Четыре одинаковых  |              <---  \n" +
-            "|_____________________|_______|___________|\n" +
-            "|\t\t\t\t\t  |       |           |\n" +
-            "|      Фулл-хаус      |              <---  \n" +
-            "|_____________________|_______|___________|\n" +
-            "|\t\t\t\t\t  |       |           |\n" +
-            "|   Маленький стрит   |              <---  \n" +
-            "|_____________________|_______|___________|\n" +
-            "|\t\t\t\t\t  |       |           |\n" +
-            "|    Большой стрит    |              <---  \n" +
-            "|_____________________|_______|___________|\n" +
-            "|\t\t\t\t\t  |       |           |\n" +
-            "|        Шанс         |              <---  \n" +
-            "|_____________________|_______|___________|\n" +
-            "|\t\t\t\t\t  |       |           |\n" +
-            "|      Единицы        |              <---  \n" +
-            "|_____________________|_______|___________|\n" +
-            "|\t\t\t\t\t  |       |           |\n" +
-            "|      Yahtzee        |              <---  \n" +
-            "|_____________________|_______|___________|\n" +
-            "|\t\t\t\t\t  |       |           |\n" +
-            "|      ОБЩИЙ СЧЕТ     |  " + SummaOfAllNumbersPerson + "       " + SummaOfAllNumbersBot + "    <---  \n" +
-            "|_____________________|_______|___________|";
+    final String one =
+            """
+                    -----
+                    |   |
+                    | o |
+                    |   |
+                    -----""";
+    final String two =
+            """
+                    -----
+                    |o  |
+                    |   |
+                    |  o|
+                    -----""";
+    final String three =
+            """
+                    -----
+                    |o  |
+                    | o |
+                    |  o|
+                    -----""";
+    final String four =
+            """
+                    -----
+                    |o o|
+                    |   |
+                    |o o|
+                    -----""";
+    final String five =
+            """
+                    -----
+                    |o o|
+                    | o |
+                    |o o|
+                    -----""";
 
-    public void CubeRandom() {
+    final String six =
+            """
+                    -----
+                    |o o|
+                    |o o|
+                    |o o|
+                    -----""";
+
+    public static void updateArea() {
+        area =
+                "------------------------------------\n" +
+                        "|\t\t\t\t      | Игрок | Бот |\n" +
+                        "|_____________________|_______|_____|_____     \n" +
+                        "|\t\t\t\t\t  |       |           |\n" +
+                        "|      Единицы        |              <---  \n" +
+                        "|_____________________|_______|___________|\n" +
+                        "|\t\t\t\t\t  |       |           |\n" +
+                        "|       Двойки        |              <---\n" +
+                        "|_____________________|_______|___________|\n" +
+                        "|\t\t\t\t\t  |       |           |\n" +
+                        "|       Тройки        |              <--- \n" +
+                        "|_____________________|_______|___________|\n" +
+                        "|\t\t\t\t\t  |       |           |\n" +
+                        "|      Четверки       |              <---  \n" +
+                        "|_____________________|_______|___________|\n" +
+                        "|\t\t\t\t\t  |       |           |\n" +
+                        "|      Пятерки        |              <---  \n" +
+                        "|_____________________|_______|___________|\n" +
+                        "|\t\t\t\t\t  |       |           |\n" +
+                        "|      Шестерки       |              <---  \n" +
+                        "|_____________________|_______|___________|\n" +
+                        "|\t\t\t\t\t  |       |           |\n" +
+                        "|        Сумма        |              <---  \n" +
+                        "|_____________________|_______|___________|\n" +
+                        "|\t\t\t\t\t  |       |           |\n" +
+                        "|        Бонус        |              <---  \n" +
+                        "|_____________________|_______|___________|\n" +
+                        "|\t\t\t\t\t  |       |           |\n" +
+                        "|   Три одинаковых    |              <---  \n" +
+                        "|_____________________|_______|___________|\n" +
+                        "|\t\t\t\t\t  |       |           |\n" +
+                        "|  Четыре одинаковых  |              <---  \n" +
+                        "|_____________________|_______|___________|\n" +
+                        "|\t\t\t\t\t  |       |           |\n" +
+                        "|      Фулл-хаус      |              <---  \n" +
+                        "|_____________________|_______|___________|\n" +
+                        "|\t\t\t\t\t  |       |           |\n" +
+                        "|   Маленький стрит   |              <---  \n" +
+                        "|_____________________|_______|___________|\n" +
+                        "|\t\t\t\t\t  |       |           |\n" +
+                        "|    Большой стрит    |              <---  \n" +
+                        "|_____________________|_______|___________|\n" +
+                        "|\t\t\t\t\t  |       |           |\n" +
+                        "|        Шанс         |              <---  \n" +
+                        "|_____________________|_______|___________|\n" +
+                        "|\t\t\t\t\t  |       |           |\n" +
+                        "|      Единицы        |              <---  \n" +
+                        "|_____________________|_______|___________|\n" +
+                        "|\t\t\t\t\t  |       |           |\n" +
+                        "|      Yahtzee        |              <---  \n" +
+                        "|_____________________|_______|___________|\n" +
+                        "|\t\t\t\t\t  |       |           |\n" +
+                        "|      ОБЩИЙ СЧЕТ     |  " + summaOfAllNumbersPerson + "     " + summaOfAllNumbersBot + "   <---  \n" +
+                        "|_____________________|_______|___________|";
+        area = String.format(area, summaOfAllNumbersPerson, summaOfAllNumbersBot);
+        System.out.println(area);
+    }
+
+    public void cubeRandom(int player) {
         Random random = new Random();
         String[] arrayCubes = new String[]{one, two, three, four, five, six};
         for (int i = 1; i <= 6; i++) {
             randomCube = arrayCubes[random.nextInt(arrayCubes.length)];
             System.out.println(randomCube);
-            PlayersScore();
-        }
-
-    }
-
-    public void PlayersScore() {
-        if (randomCube.equals(one)) {
-            SummaOfAllNumbersPerson = SummaOfAllNumbersPerson + Cube_ONE;
-        } else if (randomCube.equals(two)) {
-            SummaOfAllNumbersPerson = SummaOfAllNumbersPerson + Cube_TWO;
-        } else if (randomCube.equals(three)) {
-            SummaOfAllNumbersPerson = SummaOfAllNumbersPerson + Cube_THREE;
-        } else if (randomCube.equals(four)) {
-            SummaOfAllNumbersPerson = SummaOfAllNumbersPerson + Cube_FOUR;
-        } else if (randomCube.equals(five)) {
-            SummaOfAllNumbersPerson = SummaOfAllNumbersPerson + Cube_FIVE;
-        } else if (randomCube.equals(six)) {
-            SummaOfAllNumbersPerson = SummaOfAllNumbersPerson + Cube_SIX;
+            if (player == 1) {
+                playersPersonScore();
+            } else {
+                playersBotScore();
+            }
         }
     }
 
+    public void playersPersonScore() {
+        switch (randomCube) {
+            case one -> summaOfAllNumbersPerson = summaOfAllNumbersPerson + CUBE_ONE;
+            case two -> summaOfAllNumbersPerson = summaOfAllNumbersPerson + CUBE_TWO;
+            case three -> summaOfAllNumbersPerson = summaOfAllNumbersPerson + CUBE_THREE;
+            case four -> summaOfAllNumbersPerson = summaOfAllNumbersPerson + CUBE_FOUR;
+            case five -> summaOfAllNumbersPerson = summaOfAllNumbersPerson + CUBE_FIVE;
+            case six -> summaOfAllNumbersPerson = summaOfAllNumbersPerson + CUBE_SIX;
+        }
+    }
+    public void playersBotScore() {
+        switch (randomCube) {
+            case one -> summaOfAllNumbersBot = summaOfAllNumbersBot + CUBE_ONE;
+            case two -> summaOfAllNumbersBot = summaOfAllNumbersBot + CUBE_TWO;
+            case three -> summaOfAllNumbersBot = summaOfAllNumbersBot + CUBE_THREE;
+            case four -> summaOfAllNumbersBot = summaOfAllNumbersBot + CUBE_FOUR;
+            case five -> summaOfAllNumbersBot = summaOfAllNumbersBot + CUBE_FIVE;
+            case six -> summaOfAllNumbersBot = summaOfAllNumbersBot + CUBE_SIX;
+        }
+    }
 }
+
--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,11 +1,16 @@
 /**
  * @author Evgeniy_Tyslevich
- * @link https://github.com/dublXq
+ * @link <a href="https://github.com/dublXq">...</a>
  */
 public class Launcher {
-
     public static void main(String[] args) {
 
+        GameSystem gameSystem = new GameSystem();
+        gameSystem.cubeRandom(1);
+        System.out.println("\n---------------------------------------\n");
+        GameSystem.updateArea();
 
     }
+
+
 }
